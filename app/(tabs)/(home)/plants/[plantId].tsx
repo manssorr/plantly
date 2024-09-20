6a3299c03acae9fd85faeedf208a3e23410bb1c1
Mansour Koura <mansour.koura@vodafone.com>
2024-09-20 17:14:41 +0300

    Add initial route name and handle water via param

--- app/(tabs)/(home)/plants/[plantId].tsx
+++ app/(tabs)/(home)/plants/[plantId].tsx
@@ -22,6 +22,14 @@ export default function PlantDetails() {
   );
 
   useEffect(() => {
+    if (params.action === "water") {
+      if (typeof plantId === "string") {
+        waterPlant(plantId);
+      }
+    }
+  }, []);
+
+  useEffect(() => {
     navigation.setOptions({
       title: plant?.name,
     });
