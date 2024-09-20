bba88d4792ab714b314ae1393d1b1000ff0cc093
Mansour Koura <mansour.koura@vodafone.com>
2024-09-20 11:43:21 +0300

    A form for adding a new plant

--- app/new.tsx
+++ app/new.tsx
@@ -1,19 +1,118 @@
-import { View, Text, StyleSheet } from "react-native";
+import {
+  View,
+  Text,
+  StyleSheet,
+  TextInput,
+  Alert,
+  ScrollView,
+} from "react-native";
 import { theme } from "@/theme";
+import { useState } from "react";
+import { PlantlyImage } from "@/components/PlantltImage";
+import { PlantlyButton } from "@/components/PlantlyButton";
+import { KeyboardAwareScrollView } from "react-native-keyboard-aware-scroll-view";
 
 export default function NewScreen() {
+  const [name, setName] = useState<string>("");
+  const [days, setDays] = useState<string>("");
+
+  const handleSubmit = () => {
+    if (!name) {
+      return Alert.alert("Validation Error", "Give your plant a name");
+    }
+
+    if (!days) {
+      return Alert.alert(
+        "Validation Error",
+        `How often does ${name} need to be watered?`,
+      );
+    }
+
+    if (Number.isNaN(Number(days))) {
+      return Alert.alert(
+        "Validation Error",
+        "Watering frequency must be a be a number",
+      );
+    }
+
+    console.log("Adding plant", name, days);
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>New plant</Text>
-    </View>
+    <KeyboardAwareScrollView
+      style={styles.container}
+      contentContainerStyle={styles.contentContainer}
+      keyboardShouldPersistTaps="handled"
+    >
+      <View style={styles.centered}>
+        <PlantlyImage />
+      </View>
+      <Input
+        label="Name"
+        placeholder="E.g. 1"
+        value={name}
+        onChangeText={setName}
+      />
+      <Input
+        label="Water frequency (every x days)"
+        placeholder="E.g. 1"
+        value={days}
+        onChangeText={setDays}
+      />
+      <PlantlyButton title="Add plant" onPress={handleSubmit} />
+    </KeyboardAwareScrollView>
   );
 }
+
+const Input = ({
+  label,
+  placeholder,
+  value,
+  onChangeText,
+}: {
+  label: string;
+  placeholder: string;
+  value: string;
+  onChangeText: (text: string) => void;
+}) => {
+  return (
+    <>
+      <Text style={styles.label}>{label}</Text>
+      <TextInput
+        value={value}
+        onChangeText={onChangeText}
+        style={styles.input}
+        placeholder={placeholder}
+        autoCapitalize="words"
+      />
+    </>
+  );
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: theme.colorWhite,
-    justifyContent: "center",
+  },
+  contentContainer: {
+    paddingTop: 24,
+    paddingHorizontal: 24,
+    paddingBottom: 100,
+  },
+
+  label: {
+    fontSize: 18,
+    marginBottom: 8,
+  },
+  input: {
+    borderWidth: 2,
+    borderColor: theme.colorLightGrey,
+    padding: 12,
+    borderRadius: 6,
+    marginBottom: 24,
+    fontSize: 18,
+  },
+  centered: {
     alignItems: "center",
   },
 });
--- theme.ts
+++ theme.ts
@@ -3,5 +3,6 @@ export const theme = {
   colorLeafyGreen: "#206a42",
   colorAppleGreen: "#a0d36c",
   colorLimeGreen: "#d0e57e",
+  colorLightGrey: "#eee",
   colorWhite: "#fff",
 };
