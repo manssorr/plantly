b33cf0f8665be35942ce37926fd1787097618b5c
Mansour Koura <mansour.koura@vodafone.com>
2024-09-20 09:45:54 +0300

    Set up bottom tabs

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,14 +1,49 @@
-import { Stack } from "expo-router";
+import { Entypo, Feather } from "@expo/vector-icons";
+import { Tabs } from "expo-router";
+import { theme } from "../theme";
+import { Text } from "react-native";
+
+const tabBarLabel = ({
+  focused,
+  color,
+  children,
+}: {
+  focused: boolean;
+  color: string;
+  children: string;
+}) => {
+  return focused ? (
+    <Text style={{ color, fontSize: 12, fontWeight: "bold" }}>{children}</Text>
+  ) : null;
+};
 
 export default function RootLayout() {
   return (
-    <Stack>
-      <Stack.Screen
+    <Tabs
+      screenOptions={{
+        tabBarActiveTintColor: theme.colorGreen,
+      }}
+    >
+      <Tabs.Screen
         name="index"
         options={{
-          title: "Plantly",
+          title: "Home",
+          tabBarIcon: ({ color, size }) => (
+            <Entypo name="leaf" size={size} color={color} />
+          ),
+          tabBarLabel,
         }}
       />
-    </Stack>
+      <Tabs.Screen
+        name="profile"
+        options={{
+          title: "Profile",
+          tabBarIcon: ({ color, size }) => (
+            <Feather name="user" size={size} color={color} />
+          ),
+          tabBarLabel,
+        }}
+      />
+    </Tabs>
   );
 }
