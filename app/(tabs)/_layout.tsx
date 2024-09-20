733b21a8c435c47bf7ccdcf1f70c4f13116d7d70
Mansour Koura <mansour.koura@vodafone.com>
2024-09-20 10:42:45 +0300

    Persist onboarding state in async storage

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -14,7 +14,6 @@ export default function RootLayout() {
     return <Redirect href="/onboarding" />;
   }
 
-  console.log(`hasFinishedOnboarding`, hasFinishedOnboarding);
   return (
     <Tabs
       screenOptions={{
--- store/userStore.ts
+++ store/userStore.ts
@@ -1,18 +1,25 @@
 import { create } from "zustand";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { persist, createJSONStorage } from "zustand/middleware";
 
 type UserState = {
   hasFinishedOnboarding: boolean;
   toggleHasOnboarded: () => void;
 };
 
-export const useUserStore = create<UserState>((set, get) => ({
-  hasFinishedOnboarding: false,
-  toggleHasOnboarded: () => {
-    set((state) => {
-      return {
-        ...state,
-        hasFinishedOnboarding: !state.hasFinishedOnboarding,
-      };
-    });
-  },
-}));
+export const useUserStore = create<UserState>()(
+  persist(
+    (set, get) => ({
+      hasFinishedOnboarding: false,
+      toggleHasOnboarded: () => {
+        set((state) => ({
+          hasFinishedOnboarding: !state.hasFinishedOnboarding,
+        }));
+      },
+    }),
+    {
+      name: "user-storage",
+      storage: createJSONStorage(() => AsyncStorage),
+    }
+  )
+);
