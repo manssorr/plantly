fbef2a1bf366eb67a8f59144b482e2a929905746
Mansour Koura <mansour.koura@vodafone.com>
2024-09-20 11:09:23 +0300

    Custom pressable button

--- app/onboarding.tsx
+++ app/onboarding.tsx
@@ -1,7 +1,8 @@
-import { Text, View, StyleSheet, Button } from "react-native";
+import { View, StyleSheet } from "react-native";
 import { theme } from "@/theme";
 import { useUserStore } from "@/store/userStore";
 import { useRouter } from "expo-router";
+import { PlantlyButton } from "@/components/PlantlyButton";
 
 export default function OnboardingScreen() {
   const router = useRouter();
@@ -15,7 +16,7 @@ export default function OnboardingScreen() {
 
   return (
     <View style={styles.container}>
-      <Button title="Let me in" onPress={handleOnboarding} />
+      <PlantlyButton title="Let me in" onPress={handleOnboarding} />
     </View>
   );
 }
